dc2244e466ec35c2a1d9bb64fc738aa0278b0c08
Maxlego08 <maxlego08@sfr.fr>
2023-01-28 21:57:42 +0100

    :construction: Add footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+@php use Carbon\Carbon; @endphp
+<footer>
+    <span>{{ Carbon::now()->year }}</span>
+    <span>-</span>
+    <span><a href="https://groupez.dev" target="_blank">GroupeZ</a></span>
+    <span>-</span>
+    <span><a href="https://serveur-minecraft-vote.fr/" target="_blank">Serveur Minecraft Vote</a></span>
+</footer>
