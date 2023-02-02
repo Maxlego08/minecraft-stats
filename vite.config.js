c4623e8a2f069d9004268eadf7b115e5f2d1610f
Maxlego08 <maxlego08@sfr.fr>
2023-02-02 21:08:47 +0100

    :memo: Vite config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 // 'resources/js/app.js'
-                'resources/js/stats.js'
+                'resources/js/stats.js',
+                'resources/js/compare.js'
             ],
             refresh: true,
         }),
